d7a488c8ce8a8e4d22e329c11839a3d4b9a17f81
Michael Pellegrini <michael@cappers.ai>
2024-10-04 08:14:53 -0700

    Rendering list of tasks

---
+++ src/ToDo.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const ToDo = () => {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+
+    const toggleTaskCompletion = () => {
+
+    };
+
+    const handleInputChange = (e) => {
+        setNewTask(e.target.value);
+    };
+
+    const addTask = () => {
+        console.log('addTask newTask: ', newTask);
+        if (newTask.trim() !== '') {
+            setTasks([...tasks, {text: newTask, completed: false}]);
+            setNewTask('');
+        }
+    };
+
+    return (
+        <div style={{ marginTop: '20px' }}>
+            <h2>To Do List</h2>
+            <ul>
+                {tasks.map((task, index) => (
+                    <li key={index} className={task.completed ? 'task-completed' : ''}>
+                        <input type="checkbox" checked={task.completed} onChange={() => toggleTaskCompletion(index)} />
+                        {task.text}
+                    </li>
+                ))}
+            </ul>
+            <div style={{ marginTop: '20px' }}>
+                <input type="text" value={newTask} onChange={handleInputChange} placeholder="Enter a new task" />
+                <br />
+                <button type="button" onClick={addTask} style={{marginTop: '10px'}}>Add Task</button>
+            </div>
+        </div>
+    );
+};
+
+export default ToDo;
