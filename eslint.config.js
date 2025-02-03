e464782118e77cca2c5e5052f76c628a3ba9561e
Michael Pellegrini <michael@cappers.ai>
2025-02-03 15:57:34 -0800

    Reconfigures ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,18 @@
+import globals from "globals";
 import js from '@eslint/js'
-import globals from 'globals'
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
+
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   { ignores: ['dist'] },
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {
-    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -35,4 +40,6 @@ export default [
       ],
     },
   },
-]
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
